98aa75b4d19000167de420a2f5f304d64ef4b11c
nanonacho <ignacio.alvaradome@gmail.com>
2022-03-27 01:38:56 -0300

    Add js functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-function mostrarTrabajadores() {
+function mostrarProductos() {
     fetch('http://127.0.0.1:5000/productos')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        for (d in data) {
+            document.getElementById("productos").insertAdjacentHTML("beforeend", `
+            <tr>
+                <td>${data[d].id}</td>
+                <td>${data[d].nombre}</td>
+                <td>${data[d].categoria}</td>
+                <td>${data[d].precio}</td>
+            </tr>`)
+            console.log(data[d].id)
+        }
+    })
 }
