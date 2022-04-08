1de299d783a83bd338ac3be75dad2f0c6c653bc3
nanonacho <ignacio.alvaradome@gmail.com>
2022-04-07 21:25:29 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -14,3 +14,20 @@ function mostrarProductos() {
         }
     })
 }
+
+function mostrarTrabajadores() {
+    fetch('http://127.0.0.1:5000/trabajadores')
+    .then(response => response.json())
+    .then(data => {
+        for (d in data) {
+            document.getElementById("trabajadores").insertAdjacentHTML("beforeend", `
+            <tr>
+                <td>${data[d].id}</td>
+                <td>${data[d].rut}</td>
+                <td>${data[d].nombre}</td>
+                <td>${data[d].fecha_registro}</td>
+            </tr>`)
+            console.log(data[d].id)
+        }
+    })
+}
